ecf1fb8e08fad800ee4649a14da96ce2c07cf15a
rmayo298 <Robert_Mayo2@comcast.com>
2020-06-06 14:49:19 -0400

    Initial Commit to test MyGet flow.

---
+++ src/MayoSolutions.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace MayoSolutions.Common.Extensions
+{
+    public static class StringExtensions
+    {
+
+
+        [Pure]
+        public static bool EqualsCaseInsensitive(this string a, string b)
+        {
+            if (a == null && b == null) return true;
+            if (a == null) return false;
+            return a.Equals(b, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ tests/MayoSolutions.Common.UnitTests/Extensions/StringExtensions/EqualsCaseInsensitiveTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MayoSolutions.Common.Extensions;
+using NUnit.Framework;
+
+namespace MayoSolutions.Common.UnitTests.Extensions.StringExtensions
+{
+    public class EqualsCaseInsensitiveTests
+    {
+        [Test]
+        [TestCase(null, null, true)]
+        [TestCase(null, "", false)]
+        [TestCase(null, "b", false)]
+        [TestCase("", null, false)]
+        [TestCase("a", null, false)]
+        [TestCase("", "", true)]
+        [TestCase("a", "a", true)]
+        [TestCase("a", "A", true)]
+        [TestCase("a", "b", false)]
+        [TestCase("b", "b", true)]
+        [TestCase("B", "b", true)]
+        [TestCase("a", "B", false)]
+        [TestCase("A", "b", false)]
+        public void ShouldMakeCaseInsensitiveComparisons(string a, string b, bool expected)
+        {
+            Assert.AreEqual(expected, a.EqualsCaseInsensitive(b));
+        }
+    }
+}
